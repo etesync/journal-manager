f18bb4ac14207a1e0b34eacf91a42880954a63db
Tom Hacohen <tom@stosb.com>
2017-01-04 23:21:07 +0000

    Switch to drf serializers.

---
+++ journal/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from . import models
+
+class EntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Entry
+        fields = ('id', 'uuid', 'content')
--- journal/views.py
+++ journal/views.py
@@ -6,6 +6,7 @@ from django.views.generic import View
 from django.http import JsonResponse
 
 from .models import Entry
+from .serializers import EntrySerializer
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -17,19 +18,15 @@ class RestView(View):
         else:
             entries = Entry.objects.filter(id__gt=last)
 
-        ret = map(lambda x: {'id': x.id, 'content': x.content.decode(),
-                             'uuid': x.uuid},
-                            entries)
-        return JsonResponse({'entries': list(ret)})
+        serializer = EntrySerializer(entries, many=True)
+        return JsonResponse({'entries': serializer.data})
 
     @csrf_exempt
     def put(self, request):
-        entries = json.loads(request.body.decode())
-        print(entries)
-        for entry in entries['entries']:
-            Entry(content=entry['content'].encode(),
-                  uuid=entry['uuid']).save()
+        body = json.loads(request.body.decode())
+        entries = EntrySerializer(data=body)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
 
-        res = JsonResponse({'ok': 1})
-        res.status_code = 201
-        return res
+        return JsonResponse(serializer.errors, status=400)
