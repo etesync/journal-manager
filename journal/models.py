8d2d181bc3f58b3ca26c0eab500513ca35fec374
Tom Hacohen <tom@stosb.com>
2017-01-05 19:30:36 +0000

    Add db index to tag too.

--- journal/models.py
+++ journal/models.py
@@ -4,4 +4,4 @@ import uuid
 class Entry(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True)
     content = models.BinaryField(editable=True)
-    tag = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200, db_index=True)
