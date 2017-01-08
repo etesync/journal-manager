0a815d5a9da9b8f212c24f062e4239e3694c2ca1
Tom Hacohen <tom@stosb.com>
2017-01-08 17:47:01 +0000

    Add more db restrictions.

--- journal/models.py
+++ journal/models.py
@@ -1,17 +1,15 @@
-import uuid
-
 from django.db import models
 from django.conf import settings
 
 
 class Journal(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True)
+    uuid = models.UUIDField(db_index=True, unique=True, blank=False, null=False)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.BinaryField(editable=True)
+    content = models.BinaryField(editable=True, blank=False, null=False)
     deleted = models.BooleanField(default=False)
 
 
 class Entry(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True)
-    content = models.BinaryField(editable=True)
+    uuid = models.UUIDField(db_index=True, unique=True, blank=False, null=False)
+    content = models.BinaryField(editable=True, blank=False, null=False)
     journal = models.ForeignKey(Journal)
