dc716928f1ee357e99ba679c3e4bc7f071e882a1
Tom Hacohen <tom@stosb.com>
2017-04-03 16:44:47 +0100

    Tests: Add missing middleware.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -21,6 +21,16 @@ INSTALLED_APPS = [
     'tests',
 ]
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
