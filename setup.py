1db779111b3546ac963eb67c79d37d912dc6576b
Tom Hacohen <tom@stosb.com>
2019-02-13 17:40:49 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-etesync-journal',
-    version='0.5.6',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     license='AGPLv3',
